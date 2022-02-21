55fd5baf62c3a692cbfb4a81dba91ed500e7dbbb
Eduardo Francos Margareto <efmargareto@hotmail.com>
2022-02-21 21:31:45 +0100

    Crud product - styles

--- config/routes.config.js
+++ config/routes.config.js
@@ -16,12 +16,15 @@ router.post('/tienda/:id/delete', misc.tiendaDelete)
 // Productos 
 router.get('/producto/:tiendaId/create', misc.productoCreate)
 router.post('/producto/:tiendaId/create', misc.productoDoCreate)
-router.get('/producto/:id', misc.producto)
+router.get ('/producto/:id/edit', misc.productoEdit)
+router.post('/producto/:id/edit', misc.productoDoEdit)
+router.post('/producto/:id/productoDelete', misc.productoDelete)
 
 
 
 /* MISC ROUTES  */
 router.get('/', misc.home)
 router.get('/tienda/:id', misc.tienda)
+router.get('/producto/:id', misc.producto)
 
 module.exports = router 
