bc20b372c0aebd15016722c999e611cfc90e0bb5
Eduardo Francos Margareto <efmargareto@hotmail.com>
2022-03-11 21:13:58 +0100

    short tiendas

--- controllers/misc.controller.js
+++ controllers/misc.controller.js
@@ -10,7 +10,7 @@ const Comment = require('../models/comment.model');
 
 module.exports.home = (req, res, next) => {
   Tienda.find()
-    .sort({ createdAt: 'desc' })
+    // .sort({ createdAt: 'desc' })
     .then((tiendas) => res.render('misc/home', { tiendas }))
     .catch((error) => next(error));
 };
